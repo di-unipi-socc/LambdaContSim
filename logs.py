ab0a37b00a0adec90ccdf7fc5f43045655e7ac3e
Alessio Matricardi <alessiomatricardi@yahoo.com>
2022-04-25 00:29:01 +0200

    improve readability

--- logs.py
+++ logs.py
@@ -1,6 +1,9 @@
 import logging
 
 def get_logger():
+
+    # format the logging message
+    # TIME - LOG LEVEL : message to print
     logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s')
     
     # Creating an object
