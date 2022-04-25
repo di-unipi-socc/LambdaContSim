d22415d44be084f1352ec155d967defbcc13e76f
Alessio Matricardi <alessiomatricardi@yahoo.com>
2022-04-25 04:39:56 +0200

    write report on a json file, improve code quality, rewrite crash/resurrection system

--- infrastructure/infrastructure.py
+++ infrastructure/infrastructure.py
@@ -17,29 +17,60 @@ class Infrastructure :
         self.latencies = latencies
         self.other_data = other_data
 
-    def simulate_node_crash(self):
+
+    def simulate_node_crash(self) :
         keys = self.nodes.keys()
+        
+        # get the list of active nodes
         active_nodes = []
         for key in keys:
             if self.nodes.get(key).available:
                 active_nodes.append(key)
+        
+        # if there is no active nodes, then no one can crash
+        if len(active_nodes) == 0:
+            return None
+        
         # choice a random node to kill
         node_to_kill = random.choice(active_nodes)
         self.nodes.get(node_to_kill).available = False
+        
         # save into the list of crashed nodes
         self.crashed_nodes.append(node_to_kill)
-        print("Node %s crashed" % node_to_kill)
-    
+        
+        return node_to_kill
+
+
+    def simulate_node_resurrection(self) :
+        # if there is no crashed nodes, then no one can resurrect
+        if len(self.crashed_nodes) == 0:   
+            return None
+
+        # choose a node to resurrect
+        node_to_resurrect = random.choice(self.crashed_nodes)
+        self.nodes.get(node_to_resurrect).available = True
+        
+        # remove from the list of crashed nodes
+        self.crashed_nodes.remove(node_to_resurrect)
+        
+        return node_to_resurrect
+
+
     def simulate_link_crash(self):
+        # get active nodes
         keys = self.nodes.keys()
         active_nodes = []
         for key in keys:
             if self.nodes.get(key).available:
                 active_nodes.append(key)
+        
+        # if there aren't then we can't make a link crash
         if len(active_nodes) == 0:
-            return
+            return None, None
+        
         # until we found an active link
         while True:
+            
             # choice a random node
             first_node = random.choice(active_nodes)
             second_keys = self.latencies.get(first_node).keys()
@@ -48,33 +79,32 @@ class Infrastructure :
                 if self.nodes.get(key).available:
                     second_active_nodes.append(key)
             if len(second_active_nodes) == 0:
-                break
+                return None, None
+            
             second_node = random.choice(second_active_nodes)
+            
+            # make the link unavailable
             self.latencies.get(first_node).get(second_node)['available'] = False
+            
             # save into the list of crashed links
             self.crashed_links.append({'first' : first_node, 'second' : second_node})
-            print("Link %s <-> %s crashed" % (first_node, second_node))
-            break
+            
+            return first_node, second_node
 
-    def simulate_node_resurrection(self):
-        if len(self.crashed_nodes) == 0:
-            print("No node to resurrect")
-            return
-
-        choice = random.choice(self.crashed_nodes)
-        self.nodes.get(choice).available = True
-        # remove from the list of crashed nodes
-        self.crashed_nodes.remove(choice)
-        print("Node %s resurrect" % choice)
 
     def simulate_link_resurrection(self):
+        # if there are no crashed links, then we can't resurrect one
         if len(self.crashed_links) == 0:
-            print("No links to resurrect")
-            return
+            return None, None
         
-        dictionary = random.choice(self.crashed_links)
-        self.latencies.get(dictionary['first']).get(dictionary['second'])['available'] = True
-        # remove from the list of crashed nodes
-        self.crashed_links.remove(dictionary)
-        print("Link %s <-> %s resurrect" % (dictionary['first'], dictionary['second']))
+        # choice the link to resurrect
+        link = random.choice(self.crashed_links)
+        
+        # make it available
+        self.latencies.get(link['first']).get(link['second'])['available'] = True
+        
+        # remove from the list of crashed links
+        self.crashed_links.remove(link)
+        
+        return link['first'], link['second']
         
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
 import sys, random
 from datetime import datetime
-from swiplserver import PrologMQI
+from swiplserver import PrologError, PrologMQI
 import json
 from config import parse_config
 from infrastructure.infrastructure import Infrastructure
@@ -214,9 +214,10 @@ def main(argv):
     default_infrastructure_path = os.path.join(secfaas2fog_abs_path, 'infrastructure.pl')
 
     # statistical variables
-    # TODO improve
-    # placement results is a list of objects like {start: time, end : time, success : bool}
-    placement_results : list = []
+    # TODO better comments
+    applications_stats : dict = {}
+    node_events : list = []
+    link_events : list = []
 
     # configuration file path
     config_path = None
@@ -317,75 +318,103 @@ def main(argv):
 
         # PLACEMENT PHASE
 
-        # for each application
-        for app in applications:
-            
-            # we want to trigger a new placement request?
-            trigger = take_decision(app['placement_trigger_probability'])
-            application_path = app['path']
+        # we can place an application iff there is almost one active node
+        if len(infrastructure.nodes) > len(infrastructure.crashed_nodes):
 
-            if trigger:
-                # save application file into default path
-                shutil.copy(application_path, default_application_path)
+            # for each application
+            for app in applications:
+                
+                # we want to trigger a new placement request?
+                trigger = take_decision(app['placement_trigger_probability'])
+                application_path = app['path']
 
-                # try to place this app with SecFaas2Fog
+                # get the name of the application (file)
+                application_name = os.path.basename(application_path)
 
-                # it will reply with a valid placement iff application can be placed
-                placement = None
-                application_can_be_placed = False
-                start_time = datetime.now()
-                with PrologMQI(prolog_path_args=[
-                        "-s", default_placer_path
-                ]) as mqi:
-                    with mqi.create_thread() as prolog_thread:
-                        query = prolog_thread.query(secfaas2fog_command)
-                        end_time = datetime.now()
+                if trigger:
+                    logger.info("Placement triggered for application %s", application_name)
 
-                        if isinstance(query, list):
-                            placement = query[0] # should be a dictionary
-                            #print(placement)
+                    # save application file into default path
+                    shutil.copy(application_path, default_application_path)
 
-                            # check if the placement is a dictionary
-                            if placement is not None and isinstance(placement, dict):
-                                function_chain = find_dependencies(placement)
-                                placement = build_placement(placement)
-                                #print(placement)
-                                #print(function_chain)
+                    # try to place this app with SecFaas2Fog
 
-                            application_can_be_placed = True
-                        else:
-                            print("Placement failed")
+                    # it will reply with a valid placement iff application can be placed
+                    raw_placement = None
+                    application_can_be_placed = False
+                    query = False
+                    
+                    # save SecFaas2Fog starting time
+                    start_time = datetime.now()
+                    
+                    with PrologMQI(prolog_path_args=[
+                            "-s", default_placer_path
+                    ]) as mqi:
+                        with mqi.create_thread() as prolog_thread:
 
-                placement_data : dict = {
-                    'start' : start_time,
-                    'end' : end_time,
-                    'success' : application_can_be_placed
-                }
+                            try:
+                            
+                                query = prolog_thread.query(secfaas2fog_command)
+                            
+                            except PrologError:
+                                logger.error("Prolog execution failed")
+                            
+                            finally:
+                                # save SecFaas2Fog finish time
+                                end_time = datetime.now()
 
-                placement_results.append(placement_data)
+                    if query != False and isinstance(query, list):
+                                    
+                        raw_placement = query[0] # it is a dictionary
 
-                if (application_can_be_placed) :
-                    pass
+                        function_chain = find_dependencies(raw_placement)
+                        placement = build_placement(raw_placement)
+                        
+                        #print(placement)
+                        #print(function_chain)
 
-                    # create application instance
-                    # TODO actually we have 1 application so it is defined above
+                        application_can_be_placed = True
 
-                    # place application over the infrastructure (update nodes capabilities: memory, number of vCPUs )
-                    functions = placement.keys()
-                    for function in functions:
-                        node_id = placement[function].node_id
-                        node = nodes[node_id]
-                        #print("%s %s" % (function.id, node_id))
-                        node.take_resources(memory = placement[function].memory, v_cpu = placement[function].v_cpu) # TODO functions are not padded
+                        logger.info("Placement found for %s", application_name)
+                    else:
+                        logger.info("Placement failed for %s", application_name)
+                    
+                    if application_name not in applications_stats.keys():
+                        applications_stats[application_name] = {}
+                        applications_stats[application_name]['placements'] = []
 
-                    # launch application for a determined amount of time
-                    thread = Orchestrator("Placement", 1000 + index, nodes, function_chain, placement)
-                    thread.start()
+                    # normal placement
+                    placement_data : dict = {
+                        'event' : "trigger",
+                        'start' : start_time,
+                        'end' : end_time,
+                        'success' : application_can_be_placed
+                    }
 
-            # update infastructure.pl
-            update_infrastructure(infrastructure, default_infrastructure_path)
+                    applications_stats[application_name]['placements'].append(placement_data)
 
-        # CRASH PHASE
+                    if (application_can_be_placed) :
+                        pass
+
+                        # create application instance
+                        # TODO actually we have 1 application so it is defined above
+
+                        # place application over the infrastructure (update nodes capabilities: memory, number of vCPUs )
+                        functions = placement.keys()
+                        for function in functions:
+                            node_id = placement[function].node_id
+                            node = nodes[node_id]
+                            #print("%s %s" % (function.id, node_id))
+                            node.take_resources(memory = placement[function].memory, v_cpu = placement[function].v_cpu) # TODO functions are not padded
+
+                        # launch application for a determined amount of time
+                        thread = Orchestrator("Placement", 1000 + index, nodes, function_chain, placement)
+                        thread.start()
+
+                # update infastructure.pl
+                update_infrastructure(infrastructure, default_infrastructure_path)
+
+        # CRASH/RESURRECTION PHASE
 
         # node crash
         node_crashed = take_decision(config.node_crash_probability)
@@ -398,49 +427,122 @@ def main(argv):
         link_resurrected = take_decision(config.link_crash_probability)
 
         if node_crashed:
-            print("NODE CRASH")
-            infrastructure.simulate_node_crash()
+            node_id = infrastructure.simulate_node_crash()
+            if node_id is not None:
+                logger.info("Node %s crashed", node_id)
+                
+                # add event to the list
+                event = {
+                    'type' : 'crash',
+                    'node_id' : node_id,
+                    'when' : datetime.now()
+                }
+                node_events.append(event)
+
+                # TODO there are affected applications?
         
         elif node_resurrected:
-            print("NODE RESURRECT")
-            infrastructure.simulate_node_resurrection()
+            node_id = infrastructure.simulate_node_resurrection()
+            if node_id is not None:
+                logger.info("Node %s resurrected", node_id)
+
+                # add event to the list
+                event = {
+                    'type' : 'resurrection',
+                    'node_id' : node_id,
+                    'when' : datetime.now()
+                }
+                node_events.append(event)
         
         if link_crashed:
-            print("LINK CRASH")
-            infrastructure.simulate_link_crash()
+            first_node, second_node = infrastructure.simulate_link_crash()
+            if first_node != None and second_node != None:
+                logger.info("Link %s <-> %s crashed", first_node, second_node)
+
+                # add event to the list
+                event = {
+                    'type' : 'crash',
+                    'first_node_id' : first_node,
+                    'second_node_id' : second_node,
+                    'when' : datetime.now()
+                }
+                link_events.append(event)
+
+                # TODO there are affected applications?
         
         elif link_resurrected:
-            print("LINK RESURRECT")
-            infrastructure.simulate_link_resurrection()
+            first_node, second_node = infrastructure.simulate_link_resurrection()
+            if first_node != None and second_node != None:
+                logger.info("Link %s <-> %s resurrected", first_node, second_node)
+
+                # add event to the list
+                event = {
+                    'type' : 'resurrection',
+                    'first_node_id' : first_node,
+                    'second_node_id' : second_node,
+                    'when' : datetime.now()
+                }
+                link_events.append(event)
         
         # update infastructure.pl
         update_infrastructure(infrastructure, default_infrastructure_path)
 
     # statistical prints
 
-    num_of_placement = 0
-    successess = 0
-    sum = 0
-    for result in placement_results:
-        print("Placement #%d:" % num_of_placement)
-        print("\tSecFaas2Fog start: ", result['start'])
-        print("\tSecFaas2Fog end: ", result['end'])
-        end_millisec = result['end'].timestamp() * 1000
-        start_millisec = result['start'].timestamp() * 1000
-        sum += end_millisec - start_millisec
-        print("\tPlaced? ", result['success'])
-        if result['success'] :
-            successess += 1
-        num_of_placement += 1
+    global_number_of_placements = 0 # total number of placements (for all applications)
+    global_successes = 0
+    global_sum = 0
 
-    print("Number of placements: %d" % len(placement_results))
-    print("Average time of SecFaas2Fog placement: " + str(float(f'{sum/num_of_placement:.2f}')) + " milliseconds")
-    print("Successess: %d" % successess)
+    applications_stats_keys = applications_stats.keys()
+
+    for app_name in applications_stats_keys:
+
+        application = applications_stats[app_name]
+        
+        # local accumulators
+        num_of_placements = len(application['placements'])
+        successes = 0
+        sum = 0
+
+        placement_results = application['placements']
+        
+        for result in placement_results:
+            
+            end_millisec = result['end'].timestamp() * 1000
+            start_millisec = result['start'].timestamp() * 1000
+            sum += end_millisec - start_millisec
+            
+            if result['success'] :
+                successes += 1
+        
+        # enrich application stats
+        application['num_of_placements'] = num_of_placements
+        application['successes'] = successes
+        application['average_secfaas2fog_execution'] = float(f'{sum/num_of_placements}')
+
+        # accumulate global variables
+        global_number_of_placements += num_of_placements
+        global_successes += successes
+        global_sum += sum
+    
+    stats_to_dump = {
+        'total_placements' : global_number_of_placements,
+        'total_successes' : global_successes,
+        'average_secfaas2fog_execution' : float(f'{global_sum/global_number_of_placements}'),
+        'node_events' : node_events,
+        'link_events' : link_events,
+        'data' : applications_stats
+    }
+
+    print("Number of placements: %d" % global_number_of_placements)
+    print("Average time of SecFaas2Fog placement: " + str(float(f'{global_sum/global_number_of_placements:.2f}')) + " milliseconds")
+    print("Successess: %d" % global_successes)
 
     print("Writing report file...")
 
-    # TODO write report file
-    
+    # TODO MAKE IT COMMAND ARG
+    with open("stats.json", 'w') as file:
+        json.dump(stats_to_dump, indent = 4, default = str, fp = file)
 
     print("Done!")
 
